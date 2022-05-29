111a92cef1560d48791d768712fa3441818382a6
aali <syedahmednoman@hotmail.com>
2022-05-29 13:18:25 +0530

    change is save() user

--- users/models.py
+++ users/models.py
@@ -9,8 +9,8 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         image=Image.open(self.image.path)
         if image.width>300 or image.height>300:
