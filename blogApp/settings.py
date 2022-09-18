f198107afdaa46e2f8700f2d9955ad5011ce586a
Vkonnected <syedahmednoman@hotmail.com>
2022-09-18 11:24:26 +0530

    First Commit

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'catpiefrog@gmail.com'#os.environ.get('user-name')
-EMAIL_HOST_PASSWORD = 'Aligarh!12345'#os.environ.get('user-pass')
+EMAIL_HOST_USER = 'vkonnected@gmail.com'#os.environ.get('user-name')
+EMAIL_HOST_PASSWORD = 'edqdmphidegppzut'#os.environ.get('user-pass')
